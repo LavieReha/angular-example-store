2c5bc7d25668d001a5478da9fa7a966801c2ca79
Lavie Reha <britta.lange@lavie-reha.de>
2022-01-02 17:11:19 +0100

    Added forms for user input

--- src/app/cart/cart.component.ts
+++ src/app/cart/cart.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { FormBuilder } from '@angular/forms';
+
 import { CartService } from '../cart.service';
 
 @Component({
@@ -9,8 +11,22 @@ import { CartService } from '../cart.service';
 export class CartComponent {
 
   items = this.cartService.getItems();
-  
+
+  // Set the checkoutForm property to a form model containing name and address fields by using the FormBuilder group() method.
+  checkoutForm = this.formBuilder.group({
+    name: '',
+    address: ''
+  });
+
   constructor(
-    private cartService: CartService
+    private cartService: CartService,
+    private formBuilder: FormBuilder,
   ) { }
+
+  onSubmit(): void {
+    // Process checkout data here
+    this.items = this.cartService.clearCart();
+    console.warn('Your order has been submitted', this.checkoutForm.value);
+    this.checkoutForm.reset();
+  }
 }
